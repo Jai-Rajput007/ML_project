27bc156c0ad13978439c044814f89a40318039f9
Jai-Rajput007 <136909371+Jai-Rajput007@users.noreply.github.com>
2025-04-10 16:19:33 +0530

    Models

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import  ModelTrainerConfig,ModelTrainer
 
 ## Input will be managed through this class, these are the inputs we are giving to the data ingestion component so it knows where to save what
 @dataclass
@@ -53,4 +53,7 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
