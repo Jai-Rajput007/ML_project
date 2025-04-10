2a96d3e8aa88810da75dc9a2680af509ed11b08c
Jai-Rajput007 <136909371+Jai-Rajput007@users.noreply.github.com>
2025-04-11 01:52:33 +0530

    Sometimes Mistake Happens

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,20 +24,19 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            ## Just change this line for any kind of input
             df = pd.read_csv('notebook/stud.csv')
             logging.info("Read the dataset as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index = False,header = True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
             logging.info("Train test split initiated")
 
-            train_set,test_set = train_test_split(df,test_size = 0.2,random_state = 42)
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
             
-            train_set.to_csv(self.ingestion_config.train_data_path,index = False,header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index = False,header = True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of the data is completed")
 
@@ -46,14 +45,14 @@ class DataIngestion:
                 self.ingestion_config.test_data_path     
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
         
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,preprocessor_path = data_transformation.initiate_data_transformation(train_data,test_data)
 
     modeltrainer = ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -33,58 +33,44 @@ class ModelTrainer:
                 test_array[:, -1]
             )
             models = {
-                "Random Forest" : RandomForestRegressor(),
-                "Decision Tree" : DecisionTreeRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "AdaBoost" : AdaBoostRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "KNeighbors" : KNeighborsRegressor(),
-                "CatBoost" : CatBoostRegressor(verbose=False),
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "AdaBoost": AdaBoostRegressor(),
+                "Linear Regression": LinearRegression(),
+                "KNeighbors": KNeighborsRegressor(),
+                "CatBoost": CatBoostRegressor(verbose=False),
                 "XGBRegressor": XGBRegressor()
-            }    
-            params={
+            }
+            params = {
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                 },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "Linear Regression":{},
-                "KNeighbors":{
-                    'n_neighbors':[5,7,9,11],
-                    # 'weights':['uniform','distance'],
-                    # 'algorithm':['ball_tree','kd_tree','brute']
+                "Linear Regression": {},
+                "KNeighbors": {
+                    'n_neighbors': [5, 7, 9, 11]
                 },
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 },
-                "CatBoost":{
-                    'depth': [6,8,10],
-                    # 'learning_rate': [0.01, 0.05, 0.1],
+                "CatBoost": {
+                    'depth': [6, 8, 10],
                     'iterations': [30, 50, 100]
                 },
-                "AdaBoost":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "AdaBoost": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
                 }
-                
             }
-            model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test = X_test,y_test = y_test,models = models,params=params)
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
@@ -93,19 +79,19 @@ class ModelTrainer:
             ]
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
-                file_path= self.model_trainer_config.trained_model_file_path,
-                obj = best_model
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
             )
 
             predicted = best_model.predict(X_test)
 
-            r2_square = r2_score(y_test,predicted)
+            r2_square = r2_score(y_test, predicted)
             return r2_square
         
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
